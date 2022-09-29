6982c1fb8e3f49ffdc6532161f6dc439c66ee89d
João Mateus Scarpa <joao.scarpa@gmail.com>
2022-09-29 16:48:48 -0300

    Status Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope defaults: { format: :json } do
+    root to: "status#index"
     resources :categories, only: [:index]
     resources :recommendations, only: [:index, :show]
   end
